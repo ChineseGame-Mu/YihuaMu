f5726c9b7e824feda605edfb04f8a27555580619
ChineseGame-Mu <muyihua@gmail.com>
2026-08-16 05:37:13 -0400

    Chinese UI: translate game mode title

--- frontend/src/GameMode.tsx
+++ frontend/src/GameMode.tsx
@@ -9,19 +9,19 @@ interface IProps {
 const GameModeE = (props: IProps): JSX.Element => {
   const rules = (
     <a href="rules.html" target="_blank">
-      rules
+      游戏规则
     </a>
   );
   if (props.gameMode === "Tractor") {
     return (
       <span>
-        升级 / <span className="red">Tractor</span> ({rules})
+        升级 / <span className="red">拖拉机</span>（{rules}）
       </span>
     );
   } else {
     return (
       <span>
-        找朋友 / <span className="red">Finding Friends</span> ({rules})
+        找朋友（{rules}）
       </span>
     );
   }
