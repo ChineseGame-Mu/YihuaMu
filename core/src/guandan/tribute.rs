30e4bf846ff7070ebf3c1f8415aeb0c78fe3c010
ChineseGame-Mu <muyihua@gmail.com>
2026-08-18 04:56:31 -0400

    test: cover finish-order to resistance flow

--- core/src/guandan/tribute.rs
+++ core/src/guandan/tribute.rs
@@ -208,4 +208,25 @@ mod tests {
         ];
         assert!(can_resist_tribute(&plan, &hands));
     }
+
+    #[test]
+    fn one_two_finish_flows_directly_into_double_joker_resistance() {
+        let table = TableConfig::new(4).unwrap();
+        let plan = four_player_tribute_plan(table, &[0, 2, 1]).unwrap();
+        assert_eq!(
+            plan,
+            TributePlan::Double {
+                givers: [1, 3],
+                receivers: [0, 2]
+            }
+        );
+
+        let hands = vec![
+            vec![card(Suit::Clubs, Rank::Two)],
+            vec![CardFace::Joker(Joker::Big)],
+            vec![card(Suit::Diamonds, Rank::Three)],
+            vec![CardFace::Joker(Joker::Big)],
+        ];
+        assert!(can_resist_tribute(&plan, &hands));
+    }
 }
