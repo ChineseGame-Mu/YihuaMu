8b67b18459892b596ed97b8271b08f2b9783e90f
ChineseGame-Mu <muyihua@gmail.com>
2026-08-20 04:52:45 -0400

    ui: add game logos to table headers

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -11,23 +11,30 @@ interface IProps {
   chatLink?: string | null;
 }
 
-const Header = (props: IProps): JSX.Element => (
-  <div>
-    <h1>
-      <GameMode gameMode={props.gameMode} />
-      &nbsp;
-      <SettingsButton />
-      <GameStatisticsButton />
-    </h1>
-    {props.chatLink !== undefined && props.chatLink !== null ? (
-      <p>
-        聊天室：{" "}
-        <a href={props.chatLink} target="_blank" rel="noreferrer">
-          {props.chatLink}
-        </a>
-      </p>
-    ) : null}
-  </div>
-);
+const Header = (props: IProps): JSX.Element => {
+  const isTractor = props.gameMode === "Tractor";
+  const logoSrc = isTractor ? "shengji-logo.svg" : "friends-logo.svg";
+  const logoAlt = isTractor ? "升级 / 拖拉机" : "找朋友";
+
+  return (
+    <div className="game-header-with-logo">
+      <img className="game-table-logo" src={logoSrc} alt={`${logoAlt} Logo`} />
+      <h1>
+        <GameMode gameMode={props.gameMode} />
+        &nbsp;
+        <SettingsButton />
+        <GameStatisticsButton />
+      </h1>
+      {props.chatLink !== undefined && props.chatLink !== null ? (
+        <p>
+          聊天室：{" "}
+          <a href={props.chatLink} target="_blank" rel="noreferrer">
+            {props.chatLink}
+          </a>
+        </p>
+      ) : null}
+    </div>
+  );
+};
 
 export default Header;
