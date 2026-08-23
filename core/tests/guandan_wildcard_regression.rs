ffd2b533137276ec6b86ae3aa779ee790954d8d1
ChineseGame-Mu <muyihua@gmail.com>
2026-08-23 19:20:11 -0400

    style(rustfmt): format Guandan wildcard regression test

--- core/tests/guandan_wildcard_regression.rs
+++ core/tests/guandan_wildcard_regression.rs
@@ -1,7 +1,5 @@
 use shengji_core::guandan::{
-    rules::classify_at_level,
-    strength::strengths_at_level,
-    CardFace, Joker, PlayPattern, Rank, Suit,
+    rules::classify_at_level, strength::strengths_at_level, CardFace, Joker, PlayPattern, Rank, Suit,
 };
 
 fn card(suit: Suit, rank: Rank) -> CardFace {
