62469f2588f58fe7dbc7a65d3a46d543ba9c0f79
ChineseGame-Mu <muyihua@gmail.com>
2026-08-24 23:04:38 -0400

    style(guandan): load 3d play pass buttons last

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -14,6 +14,7 @@ import "./game-logos.css";
 import "./guandan-settings-card-hints.css";
 import "./guandan-statusbar-restore.css";
 import "./guandan-topbar-final.css";
+import "./guandan-button-3d.css";
 
 import AppStateProvider from "./AppStateProvider";
 import WebsocketProvider from "./WebsocketProvider";
