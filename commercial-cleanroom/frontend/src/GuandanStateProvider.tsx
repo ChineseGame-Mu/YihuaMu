ed79b1cc1372d7abd26f01606d67dc074997405c
ChineseGame-Mu <muyihua@gmail.com>
2026-09-16 15:19:49 -0400

    fix(cleanroom): restore approved isolated Guandan frontend baseline

---
+++ commercial-cleanroom/frontend/src/GuandanStateProvider.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import type { JSX } from "react";
+import GuandanStartGate from "./GuandanStartGate";
+import { GuandanWebsocketContext } from "./GuandanWebsocketProvider";
+import {
+  adaptGuandanServerMessage,
+  initialGuandanTableState,
+} from "./guandanCompatibilityAdapter";
+import type { GuandanTableState } from "./guandanCompatibilityAdapter";
+
+interface GuandanStateContextValue {
+  state: GuandanTableState;
+  reset: () => void;
+}
+
+export const GuandanStateContext =
+  React.createContext<GuandanStateContextValue>({
+    state: initialGuandanTableState,
+    reset: () => {},
+  });
+
+interface GuandanStateProviderProps {
+  children: JSX.Element[] | JSX.Element;
+}
+
+const GuandanStateProvider: React.FunctionComponent<
+  React.PropsWithChildren<GuandanStateProviderProps>
+> = ({ children }) => {
+  const { lastMessage, messageSequence } = React.useContext(
+    GuandanWebsocketContext,
+  );
+  const [state, setState] = React.useState<GuandanTableState>(
+    initialGuandanTableState,
+  );
+
+  React.useEffect(() => {
+    if (lastMessage !== null) {
+      setState((current) => adaptGuandanServerMessage(current, lastMessage));
+    }
+  }, [messageSequence, lastMessage]);
+
+  const reset = React.useCallback(() => setState(initialGuandanTableState), []);
+  const value = React.useMemo(() => ({ state, reset }), [state, reset]);
+
+  return (
+    <GuandanStateContext.Provider value={value}>
+      {children}
+      <GuandanStartGate />
+    </GuandanStateContext.Provider>
+  );
+};
+
+export default GuandanStateProvider;
